3b9f96ba7a58126608a9ff6877eed476fa061e4c
derme302 <crmcderm@deakin.edu.au>
2014-04-18 17:18:34 +1000

    Initial Commit

---
+++ gms_dylib_basic.gmx/extensions/Extension1/dylibBasicDebug.c
@@ -0,0 +1,18 @@
+//
+//  dylibBasicDebug.c
+//  dylibBasicDebug
+//
+//  Created by Derme on 16/04/2014.
+//  Copyright (c) 2014 Derme. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+double addFunction( double a, double b ) {
+    return a + b;
+}
+
+int stringLength( char *str ) {
+    return strlen(str);
+}
