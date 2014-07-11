9c24a9baeaebb332a4eed3cc24c4c7328decb459
derme302 <derme@Dermes-MacBook-Air.local>
2014-07-11 11:36:53 +1000

    Re-write in C++

---
+++ XCode Project/dylibBasicDebug/helloWorld.h
@@ -0,0 +1,19 @@
+//
+//  helloWorld.h
+//  dylibBasicDebug
+//
+//  Created by Christopher McDermott on 11/07/2014.
+//  Copyright (c) 2014 Derme. All rights reserved.
+//
+
+#ifndef __dylibBasicDebug__helloWorld__
+#define __dylibBasicDebug__helloWorld__
+
+#include <iostream>
+
+#endif /* defined(__dylibBasicDebug__helloWorld__) */
+
+
+namespace mainLib {
+    double addNum(double num1, double num2);
+}
