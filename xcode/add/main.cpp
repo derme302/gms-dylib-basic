c0d95753b200ff6fd8ca78fc9a2a459640470a71
derme302 <derme@Dermes-MacBook-Air.local>
2014-07-12 11:45:49 +1000

    Added new project

---
+++ xcode/add/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  add
+//
+//  Created by Christopher McDermott on 12/07/2014.
+//  Copyright (c) 2014 Derme. All rights reserved.
+//
+
+#include "main.h"
+
+
+double mainLib::addNum(double num1, double num2) {
+    return (num1 + num2);
+}
---
+++ xcode/add/main.h
@@ -0,0 +1,18 @@
+//
+//  main.h
+//  add
+//
+//  Created by Christopher McDermott on 12/07/2014.
+//  Copyright (c) 2014 Derme. All rights reserved.
+//
+
+#ifndef __add__main__
+#define __add__main__
+
+#include <iostream>
+
+#endif /* defined(__add__main__) */
+
+namespace mainLib {
+    double addNum(double num1, double num2);
+}
